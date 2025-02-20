bc2bce9b65bcbaa09c83a80bb4c88ad009260dc0
illus1ve <221061@astanait.edu.kz>
2025-02-20 19:07:29 +0500

    register and login style

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:fitness_app/services/api_service.dart';
 import 'forgot_password_screen.dart';
 import 'complete_profile_screen.dart';
 import 'home_screen.dart';
-import 'register_screen.dart'; // Импортируем экран регистрации
+import 'register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -14,88 +14,219 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
+  bool isPasswordVisible = false; // Для скрытия пароля
 
   void login() async {
-  setState(() {
-    isLoading = true;
-  });
+    setState(() => isLoading = true);
 
-  bool success = await ApiService.login(
-    usernameController.text,
-    passwordController.text,
-  );
+    bool success = await ApiService.login(
+      usernameController.text, // Используем username вместо email
+      passwordController.text,
+    );
 
-  setState(() {
-    isLoading = false;
-  });
+    setState(() => isLoading = false);
 
-  if (success) {
-    bool profileCompleted = await ApiService.checkProfileStatus();
-    
-    if (profileCompleted) {
+    if (success) {
+      bool profileCompleted = await ApiService.checkProfileStatus();
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
+        MaterialPageRoute(
+          builder: (context) => profileCompleted ? HomeScreen() : CompleteProfileScreen(),
+        ),
       );
     } else {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => CompleteProfileScreen()),
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Login error. Check your username and password."),
+          backgroundColor: Colors.red,
+        ),
       );
     }
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Ошибка входа. Проверьте логин и пароль."),
-        backgroundColor: Colors.red,
-      ),
-    );
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Вход")),
-      body: Padding(
+      body: Stack(
+        children: [
+          Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(labelText: "Логин"),
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: "Пароль"),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            isLoading
-                ? CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: login,
-                    child: Text("Войти"),
-                  ),
-            SizedBox(height: 8),
-            TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ForgotPasswordScreen()),
+            _buildToggleButtons(context, true), // true = активен "Login"
+
+            SizedBox(height: 20),
+            _buildInputField("Username", usernameController), // Изменено на username
+            _buildPasswordField(),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ForgotPasswordScreen()),
+                ),
+                child: Text("Forgot password?", style: TextStyle(color: Colors.brown, fontWeight: FontWeight.bold)),
               ),
-              child: Text("Забыли пароль?"),
             ),
-            TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RegisterScreen()), // Переход на экран регистрации
-              ),
-              child: Text("Нет аккаунта? Зарегистрироваться"),
-            ),
+            SizedBox(height: 20),
+            _buildLoginButton(),
           ],
+        ),
+      ),
+        ],
+      )
+    );
+  }
+
+    Widget _buildToggleButtons(BuildContext context, bool isLogin) {
+  return Align(
+    alignment: Alignment.topCenter, // Фиксируем сверху
+    child: Container(
+      width: double.infinity, // Растягиваем на всю ширину
+      height: 40,
+      margin: EdgeInsets.only(top: 40), // Отступ сверху
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.brown[100],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                if (isLogin) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterScreen()),
+                  );
+                }
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                  color: !isLogin ? Colors.brown[300] : Colors.transparent,
+                ),
+                child: Text(
+                  "Registration",
+                  style: TextStyle(
+                    color: !isLogin ? Colors.white : Colors.brown[700],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                if (!isLogin) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                }
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  color: isLogin ? Color.fromARGB(198, 169, 127, 100) : Colors.transparent,
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    color: isLogin ? Colors.white : Colors.brown[700],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+
+  Widget _buildInputField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)), // Цвет текста метки
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: const Color.fromARGB(255, 156, 145, 141)), // Обычная граница
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: const Color.fromARGB(198, 169, 127, 88)!, width: 2.0), // Цвет при фокусе
+        ),
+        fillColor: const Color.fromARGB(255, 255, 255, 255), // Цвет фона при активации
+        filled: true, // Включаем заливку
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
         ),
       ),
     );
   }
+
+  Widget _buildPasswordField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: TextField(
+        controller: passwordController,
+        obscureText: !isPasswordVisible,
+        decoration: InputDecoration(
+          labelText: "Password",
+            labelStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)), // Цвет текста метки
+            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: const Color.fromARGB(255, 156, 145, 141)), // Обычная граница
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: const Color.fromARGB(198, 169, 127, 88)!, width: 2.0), // Цвет при фокусе
+          ),
+          fillColor: const Color.fromARGB(255, 255, 255, 255), // Цвет фона при активации
+          filled: true, // Включаем заливку
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          suffixIcon: IconButton(
+            icon: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off),
+            onPressed: () => setState(() => isPasswordVisible = !isPasswordVisible),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+  return ElevatedButton(
+    onPressed: isLoading ? null : login,
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      minimumSize: Size(double.infinity, 40),
+      shape: RoundedRectangleBorder( // Устанавливаем закругление углов
+        borderRadius: BorderRadius.circular(12), // Радиус закругления
+      ),
+    ),
+    child: isLoading 
+      ? CircularProgressIndicator(color: Colors.white) 
+      : Text("Login"),
+  );
+}
 }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fitness_app/services/api_service.dart';
+import 'complete_profile_screen.dart'; // Страница для заполнения возраста, роста и веса
+import 'home_screen.dart';
+import 'login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -14,6 +17,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController lastNameController = TextEditingController();
   bool gender = true; // true = Мужской, false = Женский
   bool isLoading = false;
+  bool isPasswordVisible = false;
 
   void register() async {
     setState(() => isLoading = true);
@@ -27,58 +31,291 @@ class _RegisterScreenState extends State<RegisterScreen> {
       gender,
     );
 
-    setState(() => isLoading = false);
-
     if (response != null) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Регистрация успешна! Теперь войдите в систему."),
-        backgroundColor: Colors.green,
-      ));
-      Navigator.pop(context);
+      // Автоматический вход в аккаунт после регистрации
+      bool loginSuccess = await ApiService.login(usernameController.text, passwordController.text);
+
+      if (loginSuccess) {
+        bool profileCompleted = await ApiService.checkProfileStatus();
+
+        // Если профиль уже заполнен, переходим в HomeScreen
+        if (profileCompleted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+          );
+        } else {
+          // Если профиль не заполнен, переходим в CompleteProfileScreen
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => CompleteProfileScreen()),
+          );
+        }
+      }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Ошибка регистрации. Проверьте данные."),
+        content: Text("Registration error. Check the data."),
         backgroundColor: Colors.red,
       ));
     }
+
+    setState(() => isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Регистрация")),
-      body: Padding(
+      body: Stack(
+        children: [
+          Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(controller: usernameController, decoration: InputDecoration(labelText: "Логин")),
-            TextField(controller: emailController, decoration: InputDecoration(labelText: "Email")),
-            TextField(controller: passwordController, decoration: InputDecoration(labelText: "Пароль"), obscureText: true),
-            TextField(controller: firstNameController, decoration: InputDecoration(labelText: "Имя")),
-            TextField(controller: lastNameController, decoration: InputDecoration(labelText: "Фамилия")),
-            Row(
-              children: [
-                Text("Пол: "),
-                DropdownButton<bool>(
-                  value: gender,
-                  onChanged: (bool? newValue) {
-                    setState(() {
-                      gender = newValue!;
-                    });
-                  },
-                  items: [
-                    DropdownMenuItem(value: true, child: Text("Мужской")),
-                    DropdownMenuItem(value: false, child: Text("Женский")),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            isLoading ? CircularProgressIndicator() : ElevatedButton(onPressed: register, child: Text("Зарегистрироваться")),
-            TextButton(onPressed: () => Navigator.pop(context), child: Text("Уже есть аккаунт? Войти")),
+            _buildToggleButtons(context, false), // false = активен "Registration"
+
+            SizedBox(height: 32),
+            _buildInputField("Username", usernameController),
+            _buildInputField("Email", emailController),
+            _buildPasswordField(),
+            _buildInputField("Name", firstNameController),
+            _buildInputField("Last Name", lastNameController),
+            _buildGenderSwitch(),
+            SizedBox(height: 20),
+            _buildRegisterButton(),
           ],
+        ),
+      ),
+        ],
+      )
+    );
+  }
+
+  Widget _buildToggleButtons(BuildContext context, bool isLogin) {
+  return Align(
+    alignment: Alignment.topCenter, // Фиксируем сверху
+    child: Container(
+      width: double.infinity, // Растягиваем на всю ширину
+      height: 40,
+      margin: EdgeInsets.only(top: 40), // Отступ сверху
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.brown[100],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                if (isLogin) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterScreen()),
+                  );
+                }
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                  color: !isLogin ? Color.fromARGB(198, 169, 127, 100) : Colors.transparent,
+                ),
+                child: Text(
+                  "Registration",
+                  style: TextStyle(
+                    color: !isLogin ? Colors.white : Colors.brown[700],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                if (!isLogin) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                }
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  color: isLogin ? Colors.brown[300] : Colors.transparent,
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    color: isLogin ? Colors.white : Colors.brown[700],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+
+
+  Widget _buildInputField(String label, TextEditingController controller) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0), // Уменьшаем общий отступ
+    child: TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.black),
+        contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12), // Уменьшаем внутренний отступ
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: const Color.fromARGB(255, 156, 145, 141)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Color.fromARGB(198, 169, 127, 88), width: 2.0),
+        ),
+        fillColor: Colors.white,
+        filled: true,
+      ),
+      style: TextStyle(fontSize: 16), // Уменьшаем размер шрифта для компактности
+    ),
+  );
+}
+
+
+  Widget _buildPasswordField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: TextField(
+        controller: passwordController,
+        obscureText: !isPasswordVisible,
+        decoration: InputDecoration(
+          labelText: "Password",
+          labelStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12), 
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: const Color.fromARGB(255, 156, 145, 141)), // Обычная граница
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: const Color.fromARGB(198, 169, 127, 88)!, width: 2.0), // Цвет при фокусе
+        ),
+        fillColor: const Color.fromARGB(255, 255, 255, 255), // Цвет фона при активации
+        filled: true, // Включаем заливку
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          suffixIcon: IconButton(
+            icon: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off),
+            onPressed: () => setState(() => isPasswordVisible = !isPasswordVisible),
+          ),
         ),
       ),
     );
   }
+
+  Widget _buildGenderSwitch() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0), // Уменьшенный внешний отступ
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 6), // Меньший отступ
+        Container(
+          height: 40, // Уменьшаем высоту контейнера
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), // Меньший радиус
+            border: Border.all(color: Colors.brown),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => setState(() => gender = true),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8), // Уменьшенный внутренний отступ
+                    decoration: BoxDecoration(
+                      color: gender ? Color.fromARGB(198, 169, 127, 100) : Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        bottomLeft: Radius.circular(8),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Male",
+                        style: TextStyle(
+                          fontSize: 14, // Уменьшаем размер шрифта
+                          color: gender ? Colors.white :Color.fromRGBO(0, 0, 0, 1),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => setState(() => gender = false),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8), // Уменьшенный внутренний отступ
+                    decoration: BoxDecoration(
+                      color: !gender ? Color.fromARGB(198, 169, 127, 100) : Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(8),
+                        bottomRight: Radius.circular(8),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Female",
+                        style: TextStyle(
+                          fontSize: 14, // Уменьшаем размер шрифта
+                          color: !gender ? Colors.white : const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+
+
+Widget _buildRegisterButton() {
+  return ElevatedButton(
+    onPressed: isLoading ? null : register,
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      minimumSize: Size(double.infinity, 40),
+      shape: RoundedRectangleBorder( // Устанавливаем закругление углов
+        borderRadius: BorderRadius.circular(12), // Радиус закругления
+      ),
+    ),
+    child: isLoading 
+      ? CircularProgressIndicator(color: Colors.white) 
+      : Text("Registration"),
+  );
+}
+
 }
